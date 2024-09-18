56ed2010142893e43d27dfab9255cb7cbf8421bb
kandoradev <kandoradev@gmail.com>
2024-09-18 10:41:52 -0700

    Initial Commit

---
+++ learningC++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  learningC++
+//
+//  Created by Kandora Ana Santana Guzman on 18/9/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
